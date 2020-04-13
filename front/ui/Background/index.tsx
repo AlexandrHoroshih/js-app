dbde3edae9b0ec1716bd18ecf97675b8380b3f2f
Alexander Khoroshikh <xad71rus@gmail.com>
2020-04-13 16:06:17 +0300

    style(ui/background): added width and height

--- front/ui/Background/index.tsx
+++ front/ui/Background/index.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { styled } from 'linaria/react';
-import {MAIN} from '../../theme/colors';
-
+import { MAIN } from '../../theme/colors';
 
 export const Background = styled.div`
-    font-family: 'Raleway', sans-serif;
-    background: ${MAIN};
+  width: 100%;
+  height: 100%;
+  font-family: 'Raleway', sans-serif;
+  background: ${MAIN};
 `;
