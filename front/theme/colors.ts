bfe83108144f9f5157caf82048b84b86b81b80a7
Alexander Khoroshikh <xad71rus@gmail.com>
2020-04-13 16:03:22 +0300

    refactor: button component

--- front/theme/colors.ts
+++ front/theme/colors.ts
@@ -1,1 +1,4 @@
+import { linearGradient } from 'polished';
+
 export const MAIN = '#E3EDF7';
+export const DARK = '#8B9EB0';
--- front/ui/Button/Button.stories.tsx
+++ front/ui/Button/Button.stories.tsx
@@ -1,33 +1,32 @@
 import * as React from 'react';
 import { Button } from '.';
-import { Block } from '../Block';
-import { text, boolean, number } from "@storybook/addon-knobs";
+import { text, boolean, number } from '@storybook/addon-knobs';
 
-export default {title: 'Button'};
+import { Background } from '../Background';
+
+export default { title: 'UI/Button' };
 
 export const standartButton = () => (
-    <>
-        <Button
-            onClick={(e) => console.log('ha', e)}
-            disabled={boolean('Disabled', false)}
-        >
-            {text('Button text', 'Click me')}
-        </Button>
-
-        <Block>
-            <Button>
-                {text('Button text', 'Click me')}
-            </Button>
-        </Block>
-    </>
+  <>
+    <Background style={{ padding: 20 }}>
+      <Button
+        onClick={(e) => console.log('ha', e)}
+        disabled={boolean('Disabled', false)}
+        primary={boolean('Primary', false)}
+        expanded={boolean('Expanded', false)}
+      >
+        {text('Button text', 'Click me')}
+      </Button>
+    </Background>
+  </>
 );
 standartButton.story = {
-    parameters: {
-        info: {
-            text: `
+  parameters: {
+    info: {
+      text: `
             simple frinedly button
             `,
-            propTables: [Button]
-        },
-    }
-}
+      propTables: [Button],
+    },
+  },
+};
