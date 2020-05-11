abddbe7d987e1339f722bb0f2df71633ae21571d
Alexander Khoroshikh <xad71rus@gmail.com>
2020-05-11 18:07:23 +0300

    feat(global styles): fixed rem's, removed all margins
    default margins must be removed, so all elems can be easily presented as simple boxes with width and
    height, margins are breaking this approach

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -25,66 +25,77 @@ export const globals = css`
     *:before,
     *:after {
       box-sizing: inherit;
+      margin: 0;
       font-family: 'Roboto', sans-serif;
     }
 
     h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    p {
+      margin: 0;
+    }
+
+    h1,
     .as-h1 {
-      font-size: ${rem(96)}px;
+      font-size: ${rem(96)};
       font-style: normal;
       font-weight: 300;
-      line-height: ${rem(112)}px;
-      letter-spacing: -${rem(1.5)}px;
+      line-height: ${rem(112)};
+      letter-spacing: -${rem(1.5)};
     }
 
     h2,
     .as-h2 {
-      font-size: ${rem(60)}px;
+      font-size: ${rem(60)};
       font-style: normal;
       font-weight: 300;
-      line-height: ${rem(70)}px;
+      line-height: ${rem(70)};
     }
 
     h3,
     .as-h3 {
-      font-size: ${rem(48)}px;
+      font-size: ${rem(48)};
       font-style: normal;
       font-weight: normal;
-      line-height: ${rem(56)}px;
+      line-height: ${rem(56)};
     }
 
     h4,
     .as-h4 {
-      font-size: ${rem(34)}px;
+      font-size: ${rem(34)};
       font-style: normal;
       font-weight: normal;
-      line-height: ${rem(40)}px;
-      letter-spacing: ${rem(0.25)}px;
+      line-height: ${rem(40)};
+      letter-spacing: ${rem(0.25)};
     }
 
     h5,
     .as-h5 {
-      font-size: ${rem(24)}px;
+      font-size: ${rem(24)};
       font-style: normal;
       font-weight: normal;
-      line-height: ${rem(28)}px;
+      line-height: ${rem(28)};
     }
 
     h6,
     .as-h6 {
-      font-size: ${rem(20)}px;
+      font-size: ${rem(20)};
       font-style: normal;
       font-weight: 500;
-      line-height: ${rem(28)}px;
-      letter-spacing: ${rem(0.15)}px;
+      line-height: ${rem(28)};
+      letter-spacing: ${rem(0.15)};
     }
 
     p {
-      font-size: ${rem(16)}px;
+      font-size: ${rem(16)};
       font-style: normal;
       font-weight: normal;
-      line-height: ${rem(24)}px;
-      letter-spacing: ${rem(0.44)}px;
+      line-height: ${rem(24)};
+      letter-spacing: ${rem(0.44)};
     }
   }
 `;
