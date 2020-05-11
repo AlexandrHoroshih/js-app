64a75da789776a4b0cb7fb3fbf0d0b209aed2efe
Alexander Khoroshikh <xad71rus@gmail.com>
2020-05-11 22:58:44 +0300

    chore(pages/index): added Button to the test page

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,25 +1,11 @@
+import { Button } from 'components/ui/Button';
+
 const Home = () => (
   <>
     <h1>Test H1</h1>
     <h2>Test H2</h2>
     <h2 className="as-h1">Test H2 as H1</h2>
-    <div
-      style={{
-        width: 200,
-        height: 200,
-        backgroundColor: 'var(--red-base)',
-        borderRadius: 6,
-      }}
-    ></div>
-    <div
-      style={{
-        width: 200,
-        height: 200,
-        backgroundColor: 'var(--red-smooth)',
-        borderRadius: 6,
-        marginTop: 20,
-      }}
-    ></div>
+    <Button>Test</Button>
   </>
 );
 
