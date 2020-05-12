ed53856eb27f0563435165458388f6fbc06a46e9
Alexander Khoroshikh <xad71rus@gmail.com>
2020-05-12 17:38:57 +0300

    revert(storybook): removed storybook and related stuff from the project

--- front/.storybook/preview.js
+++
@@ -1,8 +0,0 @@
-import { addDecorator } from '@storybook/react';
-import { withKnobs } from "@storybook/addon-knobs";
-import { withInfo } from '@storybook/addon-info';
-import '@storybook/addon-console';
-
-
-addDecorator(withInfo({inline: true}));
-addDecorator(withKnobs);
