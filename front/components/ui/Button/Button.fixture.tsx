4f7c48b4effc7e49ed9413ab4b461426e205d8c1
Alexander Khoroshikh <xad71rus@gmail.com>
2020-05-13 17:50:34 +0300

    docs(cosmos): added padding to the Button fixture - easier to use

--- front/components/ui/Button/Button.fixture.tsx
+++ front/components/ui/Button/Button.fixture.tsx
@@ -1,10 +1,14 @@
+import * as React from 'react';
 import { useValue } from 'react-cosmos/fixture';
 
 import { Button } from '.';
 
 export default () => {
   const [disabled] = useValue('disabled', { defaultValue: false });
-  const [text] = useValue('text', { defaultValue: 'click me' });
 
-  return <Button disabled={disabled}>{text}</Button>;
+  return (
+    <div style={{ padding: 16 }}>
+      <Button disabled={disabled}>text</Button>
+    </div>
+  );
 };
