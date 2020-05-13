7d6089b014ab2dab02b09e1a1eda98ef8d3251c8
Alexander Khoroshikh <xad71rus@gmail.com>
2020-05-13 17:45:24 +0300

    fix(globalstylesprovider): fragment instead of just div

--- front/theme/GlobalStylesProvider.tsx
+++ front/theme/GlobalStylesProvider.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Box } from 'reakit/Box';
 import { css } from 'linaria';
 import { normalize, rem } from 'polished';
 import Head from 'next/head';
@@ -112,6 +111,6 @@ export const GlobalStylesProvider: React.FC = ({ children }) => (
         rel="stylesheet"
       ></link>
     </Head>
-    <Box>{children}</Box>
+    <>{children}</>
   </>
 );
