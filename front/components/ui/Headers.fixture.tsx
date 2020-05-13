b2afab1923e99566de8c5727b107b242eeec5d5e
Alexander Khoroshikh <xad71rus@gmail.com>
2020-05-13 22:26:01 +0300

    docs(headers.fixture): better docs on h1-h6 elems

--- front/components/ui/Headers.fixture.tsx
+++ front/components/ui/Headers.fixture.tsx
@@ -10,8 +10,9 @@ const Headers = () => {
       <h5>Header 5</h5>
       <h6>Header 6</h6>
       <p>
-        for all headers there are .as-h* classes to make them (or other text)
-        look like the specific header example:
+        All header tags have predefied globally styles. For all headers there
+        are .as-h* classes to make them (or other text) look like the specific
+        header, example:
       </p>
       <p style={{ color: 'red' }}>
         {'<h2 className="as-h5">H2 but looks like H5</h2>'}
