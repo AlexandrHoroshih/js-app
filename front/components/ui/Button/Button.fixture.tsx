71045c9c91a571c5e1546c88d356768137f98c96
Alexander Khoroshikh <xad71rus@gmail.com>
2020-05-19 00:26:47 +0300

    improvement(button.fixture): updated fixture

--- front/components/ui/Button/Button.fixture.tsx
+++ front/components/ui/Button/Button.fixture.tsx
@@ -2,13 +2,15 @@ import * as React from 'react';
 import { useValue } from 'react-cosmos/fixture';
 
 import { Button } from '.';
+import { StackLayout } from '../Layouts/Stack';
 
 export default () => {
   const [disabled] = useValue('disabled', { defaultValue: false });
 
   return (
-    <div style={{ padding: 16 }}>
+    <StackLayout style={{ padding: 16, maxWidth: 320 }}>
       <Button disabled={disabled}>button</Button>
-    </div>
+      <Button variant="smooth">Smooth</Button>
+    </StackLayout>
   );
 };
