77893109de4b9eb85514bac7997558bb0f717f36
Alexander Khoroshikh <xad71rus@gmail.com>
2020-05-19 23:21:16 +0300

    improvement(button.fixture): updated Button fixture with the rest vars

--- front/components/ui/Button/Button.fixture.tsx
+++ front/components/ui/Button/Button.fixture.tsx
@@ -6,11 +6,18 @@ import { StackLayout } from '../Layouts/Stack';
 
 export default () => {
   const [disabled] = useValue('disabled', { defaultValue: false });
+  const [mainColor] = useValue('button main color', {
+    defaultValue: 'var(--blue-base)',
+  });
 
   return (
     <StackLayout style={{ padding: 16, maxWidth: 320 }}>
-      <Button disabled={disabled}>button</Button>
+      <Button disabled={disabled} mainColor={mainColor}>
+        button
+      </Button>
       <Button variant="smooth">Smooth</Button>
+      <Button variant="raised">Raised</Button>
+      <Button variant="ghost">Ghost</Button>
     </StackLayout>
   );
 };
