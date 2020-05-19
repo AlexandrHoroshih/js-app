f8cbbe7ffb33f46f0b64ad3d7ab4ace96aabb60c
Alexander Khoroshikh <xad71rus@gmail.com>
2020-05-19 23:20:27 +0300

    feat(button): all btn varaints implemented in basic version
    defult one is adaptable to any provided color

--- front/components/ui/Button/index.tsx
+++ front/components/ui/Button/index.tsx
@@ -6,8 +6,22 @@ import {
 import { css, cx } from 'linaria';
 import { rem } from 'polished';
 
+import { getColoredShadow } from '../../../theme/shadows';
+import { makeColorSmooth } from '../../../theme/colors';
+
+type ButtonVariantToClassMap = {
+  default: string;
+  ghost: string;
+  smooth: string;
+  raised: string;
+};
+
+type ButtonVariant = keyof ButtonVariantToClassMap;
+
 interface Props {
-  variant?: 'default' | 'ghost' | 'smooth' | 'raised';
+  variant?: ButtonVariant;
+  mainColor?: string;
+  secondaryColor?: string;
 }
 
 type ButtonProps = ReakitButtonProps & Props;
@@ -15,7 +29,9 @@ type ButtonProps = ReakitButtonProps & Props;
 const buttonBase = css`
   display: flex;
   align-items: center;
-  padding: 8px 24px;
+
+  width: max-content;
+  padding: 12px 24px;
 
   font-size: ${rem(14)};
   font-style: normal;
@@ -27,10 +43,10 @@ const buttonBase = css`
   letter-spacing: ${rem(0.75)};
 
   cursor: pointer;
-  background: var(--blue-base);
+  background: var(--btn-main-color);
   border: none;
   border-radius: var(--b-radius-4);
-  box-shadow: var(--blue-shadow8dp);
+  box-shadow: ${getColoredShadow('var(--btn-main-color)', 8)};
   transition: transform 0.4s cubic-bezier(0.8, -0.8, 0, 2);
 
   &:hover {
@@ -39,19 +55,37 @@ const buttonBase = css`
 `;
 
 const buttonSmooth = css`
-  color: var(--blue-base);
+  color: var(--btn-main-color);
   background: var(--blue-smooth);
   box-shadow: none;
 `;
 
-const mapVariantToClass = {
+const buttonGhost = css`
+  color: var(--btn-main-color);
+  background: none;
+  border: 1px solid var(--btn-main-color);
+  box-shadow: none;
+`;
+
+const buttonRaised = css`
+  color: var(--btn-main-color);
+  background: none;
+  border: none;
+  box-shadow: var(--shadow8dp);
+`;
+
+const mapVariantToClass: ButtonVariantToClassMap = {
   default: '',
   smooth: buttonSmooth,
+  ghost: buttonGhost,
+  raised: buttonRaised,
 };
 
 export const Button = ({
   variant = 'default',
   className,
+  mainColor = 'var(--blue-base)',
+  secondaryColor = 'var(--white)',
   ...props
 }: ButtonProps) => {
   const variantClass = mapVariantToClass[variant];
@@ -60,6 +94,7 @@ export const Button = ({
     <ReakitButton
       {...props}
       className={cx(buttonBase, variantClass, className)}
+      style={{ ['--btn-main-color' as any]: mainColor }}
     />
   );
 };
