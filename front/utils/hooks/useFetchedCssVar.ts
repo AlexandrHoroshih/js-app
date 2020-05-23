fef2551c200d10d03fb36dbd8b96f91fe396ad10
Alexander Khoroshikh <xad71rus@gmail.com>
2020-05-23 13:48:00 +0300

    feat(utils/hooks): implemented useFetchedCssVar hook
    allows to fetch css custom prop value and use it in react component

---
+++ front/utils/hooks/useFetchedCssVar.ts
@@ -0,0 +1,46 @@
+import { useState, useEffect, MutableRefObject } from 'react';
+
+import { fetchCssVar } from '../css/fetchCssFromRef';
+
+const rootNode = document.documentElement || document.querySelector('body');
+
+type DependencyArray = any[];
+
+type Options = {
+  ref?: MutableRefObject<null>;
+  onFetch?: (str: string) => void;
+  depsArray?: DependencyArray;
+};
+
+/** Fetches css var from provided React ref or root node by default.
+ * returns fetched value or null if no value was fetched.
+ *
+ * @param varToFetch - css custom property string which value to fetch (pattern like "--my-css-var", see https://developer.mozilla.org/en-US/docs/Web/CSS/--*).
+ * @param Options - object with keys ref, onFetch, depsArray,
+ * where
+ * @param ref - React ref object. Not required.
+ * @param onFetch - function to call, when fetch is performed, first arg is fetched value (which can be undefined). Not required
+ * @param depsArray - array of additional deps to React.useEffect hook under the hood, which already tracks ref.current, can think like "refetch if this things change". Not required
+ *
+ */
+
+export const useFetchedCssVar = (
+  varToFetch: string,
+  { ref = { current: null }, onFetch, depsArray = [] }: Options,
+) => {
+  const [currentValue, setValue] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchedVar = fetchCssVar(varToFetch, ref.current || rootNode);
+
+    if (fetchedVar) {
+      setValue(fetchedVar);
+    }
+
+    if (typeof onFetch === 'function') {
+      onFetch(fetchedVar);
+    }
+  }, [ref.current, ...depsArray]);
+
+  return currentValue;
+};
