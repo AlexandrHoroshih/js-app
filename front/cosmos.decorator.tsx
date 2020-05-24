7008475afaf6d3e0b71526bcf4029c899ef1f8ce
Alexander Khoroshikh <xad71rus@gmail.com>
2020-05-24 19:03:42 +0300

    improvement(cosmos.decorator): updated text edit toggle

--- front/cosmos.decorator.tsx
+++ front/cosmos.decorator.tsx
@@ -5,21 +5,58 @@ import { css } from 'linaria';
 import { CommonStylesProvider } from './theme';
 
 const editable = css`
-  &[contenteditable='true'] *:hover {
+  &[data-editable='true'] *:hover {
     cursor: text;
+    box-shadow: 0 0 0 2px green inset;
+  }
+
+  &[data-editable='true'] [contenteditable='true'] {
     box-shadow: 0 0 0 2px red inset;
   }
 `;
 
-const CommonDecorator: React.FC = ({ children }) => {
+const onMove = ({ target }) => {
+  const targetEditable = target.contentEditable;
+  const isEditable = targetEditable === 'inherit' ? false : targetEditable;
+
+  if (!isEditable) {
+    target.contentEditable = true;
+  } else {
+    target.contentEditable = false;
+  }
+};
+
+const ContentEditableDecorator: React.FC = ({ children }) => {
   const [isContentEditable] = useValue('Global:text edit', {
     defaultValue: false,
   });
 
+  React.useEffect(() => {
+    if (isContentEditable) {
+      document.addEventListener('mouseup', onMove);
+    }
+
+    if (!isContentEditable) {
+      document.removeEventListener('mouseup', onMove);
+    }
+
+    return () => {
+      document.removeEventListener('mouseup', onMove);
+    };
+  }, [isContentEditable]);
+
   return (
-    <div className={editable} contentEditable={isContentEditable}>
-      <CommonStylesProvider>{children}</CommonStylesProvider>
+    <div className={editable} data-editable={isContentEditable}>
+      {children}
     </div>
+  );
+};
+
+const CommonDecorator: React.FC = ({ children }) => {
+  return (
+    <ContentEditableDecorator>
+      <CommonStylesProvider>{children}</CommonStylesProvider>
+    </ContentEditableDecorator>
   );
 };
 
