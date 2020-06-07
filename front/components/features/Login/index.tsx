4b9d6f6dc748b48cf018b5a202295b96fe21d526
Alexander Khoroshikh <xad71rus@gmail.com>
2020-06-07 18:22:46 +0300

    feat(login): implemented dummy login component

---
+++ front/components/features/Login/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+import { Button } from '../../ui/Button';
+
+interface Props {}
+
+export const Login: React.FC<Props> = () => {
+  return <Button>Join in</Button>;
+};
