023f260f16b318681f3834cdfdf72f8a5199b075
Alexander Khoroshikh <xad71rus@gmail.com>
2020-06-07 18:27:44 +0300

    feat(inputs/switch): added dummy switch files

---
+++ front/components/ui/Inputs/Switch.fixture.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { useValue } from 'react-cosmos/fixture';
+
+import { Switch } from './Switch';
+
+const SwitchFixture = () => {
+  return (
+    <div style={{ padding: 20 }}>
+      <Switch />
+    </div>
+  );
+};
+
+export default SwitchFixture;
---
+++ front/components/ui/Inputs/Switch.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { css, cx } from 'linaria';
+import { rem } from 'polished';
+
+export const Switch = () => {
+  return <div>ddd</div>;
+};
