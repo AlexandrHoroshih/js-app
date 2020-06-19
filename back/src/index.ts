bf4d76567d66ec767caf4c1bef43e3ca6fc040e3
Alexander Khoroshikh <xad71rus@gmail.com>
2020-06-19 16:59:51 +0300

    feat(back): baic set up with tsconfig, eslint, prettier. Fastify used

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,3 +1,20 @@
-import config from "../../app-config";
+import fastify from 'fastify';
 
-console.log("hi", config);
+import config from '../../app-config';
+
+const { backPort = 5000 } = config;
+
+const app: fastify.FastifyInstance = fastify({
+  logger: true,
+});
+
+const main = async () => {
+  try {
+    await app.listen(backPort);
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+main();
