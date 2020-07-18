f087cae71d3b2320bf7b86833b6568624a4dbd27
Alexander Khoroshikh <xad71rus@gmail.com>
2020-07-18 22:05:55 +0300

    revert(reatom): removed reatom

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -2,37 +2,24 @@ import * as React from 'react';
 import { AppProps } from 'next/app';
 
 import { CommonStylesProvider } from '../theme';
-import { StoreProvider } from '../store/StoreProvider';
 import { AppLayout, AppContentWrapper } from '../features/AppLayout';
 import { AppHeader } from '../features/AppHeader';
 import { AppNavigation } from '../features/AppNavigation';
 import { Login } from '../features/Login';
 
-const { useLayoutEffect } = React;
-
-const appStore = initStore();
-
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
-  useLayoutEffect(() => {
-    const client = initLoguxClient();
-
-    console.log('client', client);
-  }, []);
-
   return (
-    <StoreProvider store={appStore}>
-      <CommonStylesProvider>
-        <AppLayout>
-          <AppHeader>
-            <Login />
-          </AppHeader>
-          <AppNavigation />
-          <AppContentWrapper>
-            <Component {...pageProps} />
-          </AppContentWrapper>
-        </AppLayout>
-      </CommonStylesProvider>
-    </StoreProvider>
+    <CommonStylesProvider>
+      <AppLayout>
+        <AppHeader>
+          <Login />
+        </AppHeader>
+        <AppNavigation />
+        <AppContentWrapper>
+          <Component {...pageProps} />
+        </AppContentWrapper>
+      </AppLayout>
+    </CommonStylesProvider>
   );
 }
